39077ed3f427e7c61963f27528e2a1c06ba82039
randomhero177 <kostiavasin@yahoo.com>
2018-09-02 19:35:59 +0300

    frontend: minor fixes

--- build/js/common.js
+++ build/js/common.js
@@ -52,4 +52,51 @@ $(document).ready(function() {
 		}
 	}
 	Modal.init();
+
+
+	/*
+*
+* @elClass - string. Class name
+*
+*/
+function setAnchors(elClass){
+	let blocks = $(elClass),
+	 linkElems = $('.main-menu__anchors-item a');
+
+	blocks.each(function(i, el){
+		var curId = $(el).attr('id');
+		$(linkElems[i]).attr('href', '#' + curId);
+		smoothScroll(linkElems[i]);
+	})
+
+}
+
+// Select all links with hashes
+function smoothScroll(elem){
+  $(elem).click(function(event) {
+    event.preventDefault();
+    var target = $(this.hash);
+    if (target.length) {
+      // Only prevent default if animation is actually gonna happen
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+    }
+  });
+}
+
+(function () {
+	var block = $('.header__search-wrap'),
+	menuSearchBtn = $('.main-menu__search'),
+	menuCloseBtn = $('.header__search-btn');
+
+	menuSearchBtn.click(function(e){
+		e.preventDefault();
+		block.toggleClass('hidden')
+	});
+	menuCloseBtn.click(function(e){
+		e.preventDefault();
+		block.addClass('hidden');
+	})
+})();
 })
