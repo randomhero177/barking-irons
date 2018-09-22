e4af884ac68deddb87bc823d29dde31eacf5ca5a
random_hero177 <kostiavasin@yahoo.com>
2018-09-22 14:45:38 +0300

    frontend: chenge common.js

--- build/js/common.js
+++ build/js/common.js
@@ -1,123 +1,121 @@
-$(document).ready(function() {
-	var Modal = {
-		config: {
-			modal: '.modal-block',
-			modalInner: '.modal-block__innner',
-			buttonShow: '[data-modal="show"]',
-			buttonClose: '[data-modal="close"]',
-		},
-		show: function(id){
-			var c = Modal.config,
-			curBlock = $('#' + id),
-			inner = curBlock.find(c.modalInner);
+var Modal = {
+	config: {
+		modal: '.modal-block',
+		modalInner: '.modal-block__innner',
+		buttonShow: '[data-modal="show"]',
+		buttonClose: '[data-modal="close"]',
+	},
+	show: function(id){
+		var c = Modal.config,
+		curBlock = $('#' + id),
+		inner = curBlock.find(c.modalInner);
 
-			$(curBlock).addClass('active');
-			inner.toggle(300);
-		},
-		hide: function(id){
-			var c = Modal.config,
-			curBlock = $('#' + id),
-			inner = curBlock.find(c.modalInner);
+		$(curBlock).addClass('active');
+		inner.toggle(300);
+	},
+	hide: function(id){
+		var c = Modal.config,
+		curBlock = $('#' + id),
+		inner = curBlock.find(c.modalInner);
 
-			$(curBlock).removeClass('active');
-			inner.toggle(300);
-		},
-		buttonShow: function(){
-			var c = Modal.config;
-			$(c.buttonShow).click(function(e){
-				e.preventDefault();
-				Modal.show($(this).data('modal-id'))
-			});
-		},
-		buttonHide: function(){
-			var c = Modal.config;
-			$(c.buttonClose).click(function(e){
-				e.preventDefault();
-				Modal.hide($(this).parents(c.modal).attr('id'))
-			});
-		},
-		watch: function(){
-			var c = Modal.config;
-			$(c.modalInner).click(function(e){
-				e.stopPropagation();
-			})
-			$(c.modal).click(function(){
-				Modal.hide($(this).attr('id'));
-			});
-		},
-		init: function(){
-			Modal.buttonShow();
-			Modal.buttonHide();
-			Modal.watch();
-		}
+		$(curBlock).removeClass('active');
+		inner.toggle(300);
+	},
+	buttonShow: function(){
+		var c = Modal.config;
+		$(c.buttonShow).click(function(e){
+			e.preventDefault();
+			Modal.show($(this).data('modal-id'))
+		});
+	},
+	buttonHide: function(){
+		var c = Modal.config;
+		$(c.buttonClose).click(function(e){
+			e.preventDefault();
+			Modal.hide($(this).parents(c.modal).attr('id'))
+		});
+	},
+	watch: function(){
+		var c = Modal.config;
+		$(c.modalInner).click(function(e){
+			e.stopPropagation();
+		})
+		$(c.modal).click(function(){
+			Modal.hide($(this).attr('id'));
+		});
+	},
+	init: function(){
+		Modal.buttonShow();
+		Modal.buttonHide();
+		Modal.watch();
 	}
-	Modal.init();
+}
+Modal.init();
 
 
-	/*
+/*
 *
 * @elClass - string. Class name
 *
 */
 function setAnchors(elClass){
-	let blocks = $(elClass),
-	 linkElems = $('.main-menu__anchors-item a');
+let blocks = $(elClass),
+ linkElems = $('.main-menu__anchors-item a');
 
-	blocks.each(function(i, el){
-		var curId = $(el).attr('id');
-		$(linkElems[i]).attr('href', '#' + curId);
-		smoothScroll(linkElems[i]);
-	})
+blocks.each(function(i, el){
+	var curId = $(el).attr('id');
+	$(linkElems[i]).attr('href', '#' + curId);
+	smoothScroll(linkElems[i]);
+})
 
 }
 
 // Select all links with hashes
 function smoothScroll(elem){
-  $(elem).click(function(event) {
-    event.preventDefault();
-    var target = $(this.hash);
-    if (target.length) {
-      // Only prevent default if animation is actually gonna happen
-      $('html, body').animate({
-        scrollTop: target.offset().top
-      }, 1000);
-    }
-  });
+$(elem).click(function(event) {
+  event.preventDefault();
+  var target = $(this.hash);
+  if (target.length) {
+    // Only prevent default if animation is actually gonna happen
+    $('html, body').animate({
+      scrollTop: target.offset().top
+    }, 1000);
+  }
+});
 }
 
 (function () {
-    /**** search function ****/
-    var block = $('.header__search-wrap'),
-    menuSearchBtn = $('.main-menu__search'),
-    menuCloseBtn = $('.header__search-btn');
+  /**** search function ****/
+  var block = $('.header__search-wrap'),
+  menuSearchBtn = $('.main-menu__search'),
+  menuCloseBtn = $('.header__search-btn');
 
-    menuSearchBtn.click(function(e){
-    	e.preventDefault();
-    	block.toggleClass('hidden')
-    });
-    menuCloseBtn.click(function(e){
-    	e.preventDefault();
-    	block.addClass('hidden');
+  menuSearchBtn.click(function(e){
+  	e.preventDefault();
+  	block.toggleClass('hidden')
+  });
+  menuCloseBtn.click(function(e){
+  	e.preventDefault();
+  	block.addClass('hidden');
+  })
+
+  /**** MOBILE ICON ****/
+  var mIcon = $('#mobile-menu-icon'),
+    mMenu = $('#mobile-menu');
+  mIcon.click(function(){
+    $(this).toggleClass('active');
+    mMenu.toggleClass('hidden');
+  });
+
+  /**TOGGLE common func***/
+
+  var el = $('[data-toggle]');
+  el.each(function(i, el){
+    $(this).click(function(e){
+      var href = $(this).data('href');
+      $('#' + href).toggle(400);
+      $(this).toggleClass('in');
     })
+  })
 
-    /**** MOBILE ICON ****/
-    var mIcon = $('#mobile-menu-icon'),
-      mMenu = $('#mobile-menu');
-    mIcon.click(function(){
-      $(this).toggleClass('active');
-      mMenu.toggleClass('hidden');
-    });
-
-    /**TOGGLE common func***/
-
-    var el = $('[data-toggle]');
-    el.each(function(i, el){
-      $(this).click(function(e){
-        var href = $(this).data('href');
-        $('#' + href).toggle(400);
-        $(this).toggleClass('in');
-      })
-    })
-
-  })();
-})
+})();
--- build/js/pages/about.js
+++ build/js/pages/about.js
@@ -1,51 +1,26 @@
-$(document).ready(function() {
-	function setAnchors(elClass){
-		let blocks = $(elClass),
-		 linkElems = $('.main-menu__anchors-item a');
-
-		blocks.each(function(i, el){
-			var curId = $(el).attr('id');
-			$(linkElems[i]).attr('href', '#' + curId);
-			smoothScroll(linkElems[i]);
-		})
-
-	}
-	
-function smoothScroll(elem){
-  $(elem).click(function(event) {
-    event.preventDefault();
-    var target = $(this.hash);
-    if (target.length) {
-      // Only prevent default if animation is actually gonna happen
-      $('html, body').animate({
-        scrollTop: target.offset().top
-      }, 1000);
-    }
-  });
-}
-	
-    var About = {
-      config: {
-        sliderBlock: $('.about__slider'),
-      },
-      slider: function(){
-        var c = About.config;
-        c.sliderBlock.each(function(i, el){
-          let curId = $(this).attr('id');
-          $('#' + curId).owlCarousel({
-            loop:true,
-            items:1,
-            nav:true,
-            dots: 0
-          })
+$(document).ready(function() {	
+  var About = {
+    config: {
+      sliderBlock: $('.about__slider'),
+    },
+    slider: function(){
+      var c = About.config;
+      c.sliderBlock.each(function(i, el){
+        let curId = $(this).attr('id');
+        $('#' + curId).owlCarousel({
+          loop:true,
+          items:1,
+          nav:true,
+          dots: 0
         })
-      },
-      init: function(){
-          About.slider();
-          
-		  
-		  setTimeout(function(){ setAnchors('.about'); }, 1000);
-      }
+      })
+    },
+    init: function(){
+        About.slider();
+        
+	  
+	  setTimeout(function(){ setAnchors('.about'); }, 1000);
     }
-    About.init();
+  }
+  About.init();
 });
