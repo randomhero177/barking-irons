ac23be77e97f04b8d46ba21a82ab7f5c55cfbe78
randomhero177 <kostiavasin@yahoo.com>
2018-09-29 18:13:27 +0300

    frontend: add amount constructor and styles

--- build/js/common.js
+++ build/js/common.js
@@ -53,7 +53,46 @@ $(document).ready(function() {
 		}
 	}
 	Modal.init();
+
 });
+var resources = {
+			ErrorMsg_NoDescription: 'Ошибка без описания',
+			ServerError: 'К сожалению,на сервере произошла ошибка. Попробуйте повторить операцию позднее и, если проблема повторится, <a href="/support?pid=20895" title="Создать обращение  в службу поддержки">сообщите нам</a>',
+			Shared_MaxValue: 'Максимальное значение',
+			Shared_MinValue: 'Миниимальное значение',
+			Shared_ClosestValue: 'Ближайшее значение',
+			Shared_Next: 'Вперед',
+			Shared_Prev: 'Назад',
+            Shared_Required: 'Поле обязательно для заполнения',
+            Shared_Incorrect_Data: 'Отправленные данные содержать ошибку. Исправьте ошибки и повторите попытку',
+			ConfirmRequired: 'Необходимо подтвердить свое согласие',
+			Shared_Action_Undone: 'Это действие нельзя отменить',
+			Common_copy: 'Копировать',
+			Common_delete: 'Удалить',
+			Shared_BtnDownload: 'Скачать',
+			Shared_Rename: 'Переименовать',
+			Shared_Move: 'Переместить',
+			Shared_Edit: 'Редактировать',
+			Shared_Confirm_Title: 'Подтверждение действия',
+			Shared_Confirm: 'Подтвердить',
+            Shared_Cancel: 'Отменить',
+			Sending: 'Отправляем...',
+            Sent: 'Успешно отправлено',
+			Completed_Successful_Text: 'Успешно выполнено',
+			Shared_Changes_Saved: 'Изменения сохранены успешно',
+            TurnOn: 'Включено',
+            TurnOff: 'Выключено',
+			Required: 'Поле обязательно для заполнения',
+            ServerErrorTitle: 'Ошибка сервера',
+			HtmlTagNotAvaliable: 'HTML-теги не допускаются',
+			Upload: {
+				MaxUploadFileSize: 10485760,
+				MaxUploadFileSizeErrorText: 'Разрешаются только файлы со следующими расширениями: PNG, DOC, JPEG, GIF, DOC, DOCX, TXT, RTF, PDF и размером не более 10 MB',
+				MaxUploadFiles: 10,
+				MaxUploadFilesErrorText: 'Максимальное количество загружаемых файлов за один раз: 10',
+				AllowedFileExtensionsForUpload: 'png,jpeg,jpg,gif,doc,docx,txt,rtf,pdf,zip,rar,7z,tar.bz2,tar.gz,tar,xls,xlsx,csv'
+			}
+		};
 
 /*
 *
@@ -134,3 +173,150 @@ function equalHeights(elements) {
   });
 
 })();
+
+/**
+* Constructor for increase/decrease element. Must have html-structure like in "OnceCloud.Web.Panel\Views\UnocCompany\Create.cshtml"
+* Requires jQuery
+* @param {node} obj - DOM object of amount block
+* @param {function} cbIncrease - callback function for clicking on increase button (optional)
+* @param {function} cbDecrease - callback function for clicking on decrease button (optional)
+* @param {function} customErrFunc - extra function for handling error cases (optional)
+*/
+var Amount = function (obj, cbIncrease, cbDecrease, customErrFunc) {
+var amount = this,
+	btnIncrease = obj.querySelector('.amount__increase'),
+	btnDecrease = obj.querySelector('.amount__decrease'),
+	input = obj.querySelector('.amount__input'),
+	min = Number(input.min),
+	max = Number(input.max),
+	step = (input.step) ? Number(input.step) : 1,
+	errMsgConfig = {
+		max: resources.Shared_MaxValue + ': ',
+		min: resources.Shared_MinValue + ': ',
+		closest: resources.Shared_ClosestValue + ': '
+	};
+
+this.increaseAmount = function () {
+	var prevVal = Number(input.value);
+	if (prevVal + step > max) {
+		return false;
+	} else {
+		input.value = prevVal + step;
+		btnIncrease.disabled = (prevVal + step * 2 > max) ? true : false;
+		btnDecrease.disabled = false;
+
+		if (typeof cbIncrease === 'function') {
+			cbIncrease();
+		}
+	}
+};
+
+this.decreaseAmount = function (prevVal) {
+	var prevVal = Number(input.value);
+	if (prevVal - step < min) {
+		return false;
+	} else {
+		input.value = prevVal - step;
+		btnDecrease.disabled = (prevVal - step * 2 < min) ? true : false;
+		btnIncrease.disabled = false;
+
+		if (typeof cbDecrease === 'function') {
+			cbDecrease();
+		}
+	}
+};
+
+this.init = function () {
+	var timeout;
+	if (typeof btnIncrease === 'undefined' || typeof btnDecrease === 'undefined') {
+		return false;
+	} else {
+		if (Number(input.value) === min) {
+			btnDecrease.disabled = true;
+		}
+		if (Number(input.value) === max) {
+			btnIncrease.disabled = true;
+		}
+
+		if (typeof customErrFunc === 'function') {
+			amount.showError = customErrFunc;
+		}
+
+		btnIncrease.addEventListener('click', function (e) {
+			e.preventDefault();
+			e.stopPropagation();
+			amount.increaseAmount();
+		});
+
+		btnDecrease.addEventListener('click', function (e) {
+			e.preventDefault();
+			e.stopPropagation();
+			amount.decreaseAmount();
+		});
+
+		input.addEventListener('keyup', function (e) {
+			clearTimeout(timeout);
+			timeout = setTimeout(function () {
+				amount.checkBtn();
+				amount.inputChange();
+				cbIncrease();
+			}, 800)
+		});
+		$(input).on('change', function (e) {
+			e.preventDefault();
+			clearTimeout(timeout);
+			timeout = setTimeout(function () {
+				amount.checkBtn();
+				amount.inputChange();
+				cbIncrease();
+			}, 800)
+		});
+	}
+}
+
+this.checkBtn = function () {
+	if (Number(input.value) === min) {
+		btnDecrease.disabled = true;
+	} else {
+		btnDecrease.disabled = false;
+	};
+	if (Number(input.value) === max) {
+		btnIncrease.disabled = true;
+	} else {
+		btnIncrease.disabled = false;
+	};
+};
+
+this.inputChange = function () {
+	input.value = Math.ceil(input.value);
+	var curStep = (Number(input.value) - min) / step;
+	if (curStep % 1 !== 0) {
+		input.value = Math.ceil(curStep) * step + min;
+		amount.showError(errMsgConfig.closest + input.value);
+	};
+	if (Number(input.value) > max) {
+		input.value = max;
+		amount.showError(errMsgConfig.max + max);
+	};
+	if (Number(input.value) < min) {
+		input.value = min;
+		amount.showError(errMsgConfig.min + min);
+	};
+
+};
+
+this.showError = function (msg) {
+	amount.removeErr();
+	var parentBlock = $(input).parent();
+	$('<span class="amount-tooltip amount-tooltip--active">' + msg + '</span>').hide().insertAfter(parentBlock).fadeIn(400);
+	setTimeout(amount.removeErr, 5000);
+};
+
+this.removeErr = function () {
+	if ($(input).parent().next('.amount-tooltip').length > 0) {
+		$(input).parent().next('.amount-tooltip').remove();
+	}
+};
+
+this.init();
+}
--- build/js/pages/checkout.js
+++ build/js/pages/checkout.js
@@ -1,37 +1,14 @@
 $(document).ready(function() {
 
-  var SinglePage = {
-    config: {
-      sliderBlock: $('#single-slider'),
-      toggleEl: $('.single-page__item-toggle')
-    },
-    slider: function(){
-      var c = SinglePage.config;
-      c.sliderBlock.owlCarousel({
-        loop:true,
-        items:1,
-        nav:false,
-        video:true,
-        lazyLoad:true,
-        dots: 1,
-        dotsContainer: '#single-slider-dots',
-      })
-    },
-    toggle: function(){
-      var c = SinglePage.config,
-        el = c.toggleEl;
-      el.each(function(i, el){
-        $(this).click(function(e){
-          var href = $(this).data('href');
-          $('#' + href).toggle(400);
-          $(this).toggleClass('in');
-        })
-      })
-    },
-    init: function(){
-      SinglePage.slider();
-      SinglePage.toggle();
+  var Cart = {
+    update: function(){
+
     }
-  }
-  SinglePage.init();
+  };
+
+  $('.amount').each(function () {
+      var amount = new Amount(this, Cart.update, Cart.update);
+  });
+
+
 });
