f8a16f99fb5f0bcff5d8da7712af74a067bff9d4
random_hero177 <kostiavasin@yahoo.com>
2018-10-03 21:46:02 +0300

    frontend: search page

--- build/js/common.js
+++ build/js/common.js
@@ -1,98 +1,134 @@
 $(document).ready(function() {
-	var Modal = {
-		config: {
-			modal: '.modal-block',
-			modalInner: '.modal-block__innner',
-			buttonShow: '[data-modal="show"]',
-			buttonClose: '[data-modal="close"]',
-		},
-		show: function(id){
-			var c = Modal.config,
-			curBlock = $('#' + id),
-			inner = curBlock.find(c.modalInner);
+  var Modal = {
+    config: {
+      modal: '.modal-block',
+      modalInner: '.modal-block__innner',
+      buttonShow: '[data-modal="show"]',
+      buttonClose: '[data-modal="close"]',
+    },
+    show: function(id){
+      var c = Modal.config,
+      curBlock = $('#' + id),
+      inner = curBlock.find(c.modalInner);
 
-			$(curBlock).addClass('active');
-			inner.toggle(300);
-		},
-		hide: function(id){
-			var c = Modal.config,
-			curBlock = $('#' + id),
-			inner = curBlock.find(c.modalInner);
+      $(curBlock).addClass('active');
+      inner.toggle(300);
+    },
+    hide: function(id){
+      var c = Modal.config,
+      curBlock = $('#' + id),
+      inner = curBlock.find(c.modalInner);
 
-			$(curBlock).removeClass('active');
-			inner.toggle(300);
-		},
-		buttonShow: function(){
-			var c = Modal.config;
-			$(c.buttonShow).click(function(e){
-				e.preventDefault();
-				Modal.show($(this).data('modal-id'))
-			});
-		},
-		buttonHide: function(){
-			var c = Modal.config;
-			$(c.buttonClose).click(function(e){
-				e.preventDefault();
-				console.log('sdf');
-				Modal.hide($(this).parents(c.modal).attr('id'))
-			});
-		},
-		watch: function(){
-			var c = Modal.config;
-			$(c.modalInner).click(function(e){
-				e.stopPropagation();
-			})
-			$(c.modal).click(function(){
-				Modal.hide($(this).attr('id'));
-			});
-		},
-		init: function(){
-			Modal.buttonShow();
-			Modal.buttonHide();
-			Modal.watch();
-		}
-	}
-	Modal.init();
+      $(curBlock).removeClass('active');
+      inner.toggle(300);
+    },
+    buttonShow: function(){
+      var c = Modal.config;
+      $(c.buttonShow).click(function(e){
+        e.preventDefault();
+        Modal.show($(this).data('modal-id'))
+      });
+    },
+    buttonHide: function(){
+      var c = Modal.config;
+      $(c.buttonClose).click(function(e){
+        e.preventDefault();
+        Modal.hide($(this).parents(c.modal).attr('id'))
+      });
+    },
+    watch: function(){
+      var c = Modal.config;
+      $(c.modalInner).click(function(e){
+        e.stopPropagation();
+      })
+      $(c.modal).click(function(){
+        Modal.hide($(this).attr('id'));
+      });
+    },
+    init: function(){
+      Modal.buttonShow();
+      Modal.buttonHide();
+      Modal.watch();
+    }
+  }
+  Modal.init();
 
+  
+  (function () {
+    /**** search function ****/
+    var block = $('.header__search-wrap'),
+    menuSearchBtn = $('.main-menu__search'),
+    menuCloseBtn = $('.header__search-btn');
+
+    menuSearchBtn.click(function(e){
+      e.preventDefault();
+      block.toggleClass('hidden')
+    });
+    menuCloseBtn.click(function(e){
+      e.preventDefault();
+      block.addClass('hidden');
+    })
+
+    /**** MOBILE ICON ****/
+    var mIcon = $('#mobile-menu-icon'),
+      mMenu = $('#mobile-menu');
+    mIcon.click(function(){
+      $(this).toggleClass('active');
+      mMenu.toggleClass('hidden');
+    });
+
+    /**TOGGLE common func***/
+
+    var el = $('[data-toggle]');
+    el.each(function(i, el){
+      $(this).click(function(e){
+        var href = $(this).data('href');
+        $('#' + href).toggle(400);
+        $(this).toggleClass('in');
+      })
+    });
+
+  })();
+  
 });
 var resources = {
-			ErrorMsg_NoDescription: 'Ошибка без описания',
-			ServerError: 'К сожалению,на сервере произошла ошибка. Попробуйте повторить операцию позднее и, если проблема повторится, <a href="/support?pid=20895" title="Создать обращение  в службу поддержки">сообщите нам</a>',
-			Shared_MaxValue: 'Максимальное значение',
-			Shared_MinValue: 'Миниимальное значение',
-			Shared_ClosestValue: 'Ближайшее значение',
-			Shared_Next: 'Вперед',
-			Shared_Prev: 'Назад',
+      ErrorMsg_NoDescription: 'Ошибка без описания',
+      ServerError: 'К сожалению,на сервере произошла ошибка. Попробуйте повторить операцию позднее и, если проблема повторится, <a href="/support?pid=20895" title="Создать обращение  в службу поддержки">сообщите нам</a>',
+      Shared_MaxValue: 'Максимальное значение',
+      Shared_MinValue: 'Миниимальное значение',
+      Shared_ClosestValue: 'Ближайшее значение',
+      Shared_Next: 'Вперед',
+      Shared_Prev: 'Назад',
             Shared_Required: 'Поле обязательно для заполнения',
             Shared_Incorrect_Data: 'Отправленные данные содержать ошибку. Исправьте ошибки и повторите попытку',
-			ConfirmRequired: 'Необходимо подтвердить свое согласие',
-			Shared_Action_Undone: 'Это действие нельзя отменить',
-			Common_copy: 'Копировать',
-			Common_delete: 'Удалить',
-			Shared_BtnDownload: 'Скачать',
-			Shared_Rename: 'Переименовать',
-			Shared_Move: 'Переместить',
-			Shared_Edit: 'Редактировать',
-			Shared_Confirm_Title: 'Подтверждение действия',
-			Shared_Confirm: 'Подтвердить',
+      ConfirmRequired: 'Необходимо подтвердить свое согласие',
+      Shared_Action_Undone: 'Это действие нельзя отменить',
+      Common_copy: 'Копировать',
+      Common_delete: 'Удалить',
+      Shared_BtnDownload: 'Скачать',
+      Shared_Rename: 'Переименовать',
+      Shared_Move: 'Переместить',
+      Shared_Edit: 'Редактировать',
+      Shared_Confirm_Title: 'Подтверждение действия',
+      Shared_Confirm: 'Подтвердить',
             Shared_Cancel: 'Отменить',
-			Sending: 'Отправляем...',
+      Sending: 'Отправляем...',
             Sent: 'Успешно отправлено',
-			Completed_Successful_Text: 'Успешно выполнено',
-			Shared_Changes_Saved: 'Изменения сохранены успешно',
+      Completed_Successful_Text: 'Успешно выполнено',
+      Shared_Changes_Saved: 'Изменения сохранены успешно',
             TurnOn: 'Включено',
             TurnOff: 'Выключено',
-			Required: 'Поле обязательно для заполнения',
+      Required: 'Поле обязательно для заполнения',
             ServerErrorTitle: 'Ошибка сервера',
-			HtmlTagNotAvaliable: 'HTML-теги не допускаются',
-			Upload: {
-				MaxUploadFileSize: 10485760,
-				MaxUploadFileSizeErrorText: 'Разрешаются только файлы со следующими расширениями: PNG, DOC, JPEG, GIF, DOC, DOCX, TXT, RTF, PDF и размером не более 10 MB',
-				MaxUploadFiles: 10,
-				MaxUploadFilesErrorText: 'Максимальное количество загружаемых файлов за один раз: 10',
-				AllowedFileExtensionsForUpload: 'png,jpeg,jpg,gif,doc,docx,txt,rtf,pdf,zip,rar,7z,tar.bz2,tar.gz,tar,xls,xlsx,csv'
-			}
-		};
+      HtmlTagNotAvaliable: 'HTML-теги не допускаются',
+      Upload: {
+        MaxUploadFileSize: 10485760,
+        MaxUploadFileSizeErrorText: 'Разрешаются только файлы со следующими расширениями: PNG, DOC, JPEG, GIF, DOC, DOCX, TXT, RTF, PDF и размером не более 10 MB',
+        MaxUploadFiles: 10,
+        MaxUploadFilesErrorText: 'Максимальное количество загружаемых файлов за один раз: 10',
+        AllowedFileExtensionsForUpload: 'png,jpeg,jpg,gif,doc,docx,txt,rtf,pdf,zip,rar,7z,tar.bz2,tar.gz,tar,xls,xlsx,csv'
+      }
+    };
 
 /*
 *
@@ -104,9 +140,9 @@ let blocks = $(elClass),
  linkElems = $('.main-menu__anchors-item a');
 
 blocks.each(function(i, el){
-	var curId = $(el).attr('id');
-	$(linkElems[i]).attr('href', '#' + curId);
-	smoothScroll(linkElems[i]);
+  var curId = $(el).attr('id');
+  $(linkElems[i]).attr('href', '#' + curId);
+  smoothScroll(linkElems[i]);
 })
 
 }
@@ -138,41 +174,7 @@ function equalHeights(elements) {
   }
 }
 
-(function () {
-  /**** search function ****/
-  var block = $('.header__search-wrap'),
-  menuSearchBtn = $('.main-menu__search'),
-  menuCloseBtn = $('.header__search-btn');
 
-  menuSearchBtn.click(function(e){
-  	e.preventDefault();
-  	block.toggleClass('hidden')
-  });
-  menuCloseBtn.click(function(e){
-  	e.preventDefault();
-  	block.addClass('hidden');
-  })
-
-  /**** MOBILE ICON ****/
-  var mIcon = $('#mobile-menu-icon'),
-    mMenu = $('#mobile-menu');
-  mIcon.click(function(){
-    $(this).toggleClass('active');
-    mMenu.toggleClass('hidden');
-  });
-
-  /**TOGGLE common func***/
-
-  var el = $('[data-toggle]');
-  el.each(function(i, el){
-    $(this).click(function(e){
-      var href = $(this).data('href');
-      $('#' + href).toggle(400);
-      $(this).toggleClass('in');
-    })
-  });
-
-})();
 
 /**
 * Constructor for increase/decrease element. Must have html-structure like in "OnceCloud.Web.Panel\Views\UnocCompany\Create.cshtml"
@@ -184,138 +186,138 @@ function equalHeights(elements) {
 */
 var Amount = function (obj, cbIncrease, cbDecrease, customErrFunc) {
 var amount = this,
-	btnIncrease = obj.querySelector('.amount__increase'),
-	btnDecrease = obj.querySelector('.amount__decrease'),
-	input = obj.querySelector('.amount__input'),
-	min = Number(input.min),
-	max = Number(input.max),
-	step = (input.step) ? Number(input.step) : 1,
-	errMsgConfig = {
-		max: resources.Shared_MaxValue + ': ',
-		min: resources.Shared_MinValue + ': ',
-		closest: resources.Shared_ClosestValue + ': '
-	};
+  btnIncrease = obj.querySelector('.amount__increase'),
+  btnDecrease = obj.querySelector('.amount__decrease'),
+  input = obj.querySelector('.amount__input'),
+  min = Number(input.min),
+  max = Number(input.max),
+  step = (input.step) ? Number(input.step) : 1,
+  errMsgConfig = {
+    max: resources.Shared_MaxValue + ': ',
+    min: resources.Shared_MinValue + ': ',
+    closest: resources.Shared_ClosestValue + ': '
+  };
 
 this.increaseAmount = function () {
-	var prevVal = Number(input.value);
-	if (prevVal + step > max) {
-		return false;
-	} else {
-		input.value = prevVal + step;
-		btnIncrease.disabled = (prevVal + step * 2 > max) ? true : false;
-		btnDecrease.disabled = false;
+  var prevVal = Number(input.value);
+  if (prevVal + step > max) {
+    return false;
+  } else {
+    input.value = prevVal + step;
+    btnIncrease.disabled = (prevVal + step * 2 > max) ? true : false;
+    btnDecrease.disabled = false;
 
-		if (typeof cbIncrease === 'function') {
-			cbIncrease();
-		}
-	}
+    if (typeof cbIncrease === 'function') {
+      cbIncrease();
+    }
+  }
 };
 
 this.decreaseAmount = function (prevVal) {
-	var prevVal = Number(input.value);
-	if (prevVal - step < min) {
-		return false;
-	} else {
-		input.value = prevVal - step;
-		btnDecrease.disabled = (prevVal - step * 2 < min) ? true : false;
-		btnIncrease.disabled = false;
+  var prevVal = Number(input.value);
+  if (prevVal - step < min) {
+    return false;
+  } else {
+    input.value = prevVal - step;
+    btnDecrease.disabled = (prevVal - step * 2 < min) ? true : false;
+    btnIncrease.disabled = false;
 
-		if (typeof cbDecrease === 'function') {
-			cbDecrease();
-		}
-	}
+    if (typeof cbDecrease === 'function') {
+      cbDecrease();
+    }
+  }
 };
 
 this.init = function () {
-	var timeout;
-	if (typeof btnIncrease === 'undefined' || typeof btnDecrease === 'undefined') {
-		return false;
-	} else {
-		if (Number(input.value) === min) {
-			btnDecrease.disabled = true;
-		}
-		if (Number(input.value) === max) {
-			btnIncrease.disabled = true;
-		}
+  var timeout;
+  if (typeof btnIncrease === 'undefined' || typeof btnDecrease === 'undefined') {
+    return false;
+  } else {
+    if (Number(input.value) === min) {
+      btnDecrease.disabled = true;
+    }
+    if (Number(input.value) === max) {
+      btnIncrease.disabled = true;
+    }
 
-		if (typeof customErrFunc === 'function') {
-			amount.showError = customErrFunc;
-		}
+    if (typeof customErrFunc === 'function') {
+      amount.showError = customErrFunc;
+    }
 
-		btnIncrease.addEventListener('click', function (e) {
-			e.preventDefault();
-			e.stopPropagation();
-			amount.increaseAmount();
-		});
+    btnIncrease.addEventListener('click', function (e) {
+      e.preventDefault();
+      e.stopPropagation();
+      amount.increaseAmount();
+    });
 
-		btnDecrease.addEventListener('click', function (e) {
-			e.preventDefault();
-			e.stopPropagation();
-			amount.decreaseAmount();
-		});
+    btnDecrease.addEventListener('click', function (e) {
+      e.preventDefault();
+      e.stopPropagation();
+      amount.decreaseAmount();
+    });
 
-		input.addEventListener('keyup', function (e) {
-			clearTimeout(timeout);
-			timeout = setTimeout(function () {
-				amount.checkBtn();
-				amount.inputChange();
-				cbIncrease();
-			}, 800)
-		});
-		$(input).on('change', function (e) {
-			e.preventDefault();
-			clearTimeout(timeout);
-			timeout = setTimeout(function () {
-				amount.checkBtn();
-				amount.inputChange();
-				cbIncrease();
-			}, 800)
-		});
-	}
+    input.addEventListener('keyup', function (e) {
+      clearTimeout(timeout);
+      timeout = setTimeout(function () {
+        amount.checkBtn();
+        amount.inputChange();
+        cbIncrease();
+      }, 800)
+    });
+    $(input).on('change', function (e) {
+      e.preventDefault();
+      clearTimeout(timeout);
+      timeout = setTimeout(function () {
+        amount.checkBtn();
+        amount.inputChange();
+        cbIncrease();
+      }, 800)
+    });
+  }
 }
 
 this.checkBtn = function () {
-	if (Number(input.value) === min) {
-		btnDecrease.disabled = true;
-	} else {
-		btnDecrease.disabled = false;
-	};
-	if (Number(input.value) === max) {
-		btnIncrease.disabled = true;
-	} else {
-		btnIncrease.disabled = false;
-	};
+  if (Number(input.value) === min) {
+    btnDecrease.disabled = true;
+  } else {
+    btnDecrease.disabled = false;
+  };
+  if (Number(input.value) === max) {
+    btnIncrease.disabled = true;
+  } else {
+    btnIncrease.disabled = false;
+  };
 };
 
 this.inputChange = function () {
-	input.value = Math.ceil(input.value);
-	var curStep = (Number(input.value) - min) / step;
-	if (curStep % 1 !== 0) {
-		input.value = Math.ceil(curStep) * step + min;
-		amount.showError(errMsgConfig.closest + input.value);
-	};
-	if (Number(input.value) > max) {
-		input.value = max;
-		amount.showError(errMsgConfig.max + max);
-	};
-	if (Number(input.value) < min) {
-		input.value = min;
-		amount.showError(errMsgConfig.min + min);
-	};
+  input.value = Math.ceil(input.value);
+  var curStep = (Number(input.value) - min) / step;
+  if (curStep % 1 !== 0) {
+    input.value = Math.ceil(curStep) * step + min;
+    amount.showError(errMsgConfig.closest + input.value);
+  };
+  if (Number(input.value) > max) {
+    input.value = max;
+    amount.showError(errMsgConfig.max + max);
+  };
+  if (Number(input.value) < min) {
+    input.value = min;
+    amount.showError(errMsgConfig.min + min);
+  };
 
 };
 
 this.showError = function (msg) {
-	amount.removeErr();
-	var parentBlock = $(input).parent();
-	$('<span class="amount-tooltip amount-tooltip--active">' + msg + '</span>').hide().insertAfter(parentBlock).fadeIn(400);
-	setTimeout(amount.removeErr, 5000);
+  amount.removeErr();
+  var parentBlock = $(input).parent();
+  $('<span class="amount-tooltip amount-tooltip--active">' + msg + '</span>').hide().insertAfter(parentBlock).fadeIn(400);
+  setTimeout(amount.removeErr, 5000);
 };
 
 this.removeErr = function () {
-	if ($(input).parent().next('.amount-tooltip').length > 0) {
-		$(input).parent().next('.amount-tooltip').remove();
-	}
+  if ($(input).parent().next('.amount-tooltip').length > 0) {
+    $(input).parent().next('.amount-tooltip').remove();
+  }
 };
 
 this.init();
@@ -342,7 +344,7 @@ function sendAjaxRequest(blockId, url, obj, successFunction, errFunction, type) 
     },
     error: function (data, status) {
       console.log(data, status);
-			if (typeof errorFunction === 'function') {
+      if (typeof errorFunction === 'function') {
         errorFunction(errMsg);
       };
     }
