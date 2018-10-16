7dcbc3d720f8d5b53977da0c8a29d03e86068218
random_hero177 <kostiavasin@yahoo.com>
2018-10-16 10:19:43 +0300

    panel: shipping returns; menu product in cart fix;

--- build/js/common.js
+++ build/js/common.js
@@ -137,30 +137,31 @@ var resources = {
 *
 */
 function setAnchors(elClass){
-let blocks = $(elClass),
- linkElems = $('.main-menu__anchors-item a');
+  let blocks = $(elClass),
+   linkElems = $('.main-menu__anchors-item a');
 
-blocks.each(function(i, el){
-  var curId = $(el).attr('id');
-  $(linkElems[i]).attr('href', '#' + curId);
-  smoothScroll(linkElems[i]);
-})
-
-}
+  blocks.each(function(i, el){
+    var curId = $(el).attr('id');
+    $(linkElems[i]).attr('href', '#' + curId);
+    smoothScroll(linkElems[i]);
+  })
+};
 
 // Select all links with hashes
 function smoothScroll(elem){
-$(elem).click(function(event) {
-  event.preventDefault();
-  var target = $(this.hash);
-  if (target.length) {
-    // Only prevent default if animation is actually gonna happen
-    $('html, body').animate({
-      scrollTop: target.offset().top
-    }, 1000);
-  }
-});
-}
+  $(elem).click(function(event) {
+    event.preventDefault();
+    var target = $(this.hash);
+    if (target.length) {
+      // Only prevent default if animation is actually gonna happen
+      $('html, body').animate({
+        scrollTop: target.offset().top - $('.header').height()
+      }, 1000);
+    }
+  });
+};
+
+smoothScroll($('.goTo'));
 
 function equalHeights(elements) {
   if (elements.length > 0) {
