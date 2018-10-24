dce4a19df124c32724a93e2f4a007898aba05240
random_hero177 <kostiavasin@yahoo.com>
2018-10-24 18:53:12 +0300

    frontend: height for extra-big

--- build/js/pages/category.js
+++ build/js/pages/category.js
@@ -1,21 +1,22 @@
-$(document).ready(function() {
+$(window).load(function () {
   var Category = {
 
     setHeight: function(){
       var screenHeight = $(window).height(),
         headerHeight = $('.header').height(),
         blockHeight = screenHeight - headerHeight,
-        elem = $('category-box__item:not(.category-box__item--big)'),
+        elem = $('.category-box__item:not(.category-box__item--big)'),
         extraElem = $('.category-box__item--big'),
         smallestHeight = 0;
       
-        elem.each(function(){
-          var curHeight = $(this).height();
+        elem.each(function(i, el){
+          var curHeight = $(el).height();
           smallestHeight = (curHeight > smallestHeight) ? curHeight : smallestHeight;
-          if()
-          extraElem.height(smallestHeight*2 -1)
-        })
-      
+          console.log(curHeight);
+          console.log(el);
+        });
+      extraElem.height(smallestHeight*2 -4);
+
     },
     init: function(){
       Category.setHeight();
